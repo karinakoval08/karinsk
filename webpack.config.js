d1ece3cfa88b59776340701e5c56c80a0d13a199
karinakoval08 <130156161+karinakoval08@users.noreply.github.com>
2023-10-26 14:58:13 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,49 @@
 const path = require('path');
 const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler'
-        }
-    },
-    devServer:{
-        static: path.join(__dirname, 'src'),
-        compress: true,
-        port: 8080
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            __VUE_OPTIONS_API__: true,
-            __VUE_PROD_DEVTOOLS__: false
-        })
+  context: path.resolve(__dirname, 'src'),
+  entry: './main.js',
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[local]'
+              }
+            }
+          }
+        ]
+      }
     ]
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler'
+    }
+  },
+  devServer: {
+    static: path.join(__dirname, 'src'),
+    compress: true,
+    port: 8080
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      VUE_OPTIONS_API: true,
+      VUE_PROD_DEVTOOLS: false
+    }),
+    new VueLoaderPlugin()
+  ]
 };
